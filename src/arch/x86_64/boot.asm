cec1d0572b0ef14983d8fec6e8b42923d2bc8db7
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2016-11-18 15:09:50 +0700

    Step 2.3: Load GDT table and jump to long mode

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,4 +1,5 @@
 global start
+extern long_mode_start
 
 section .text
 bits 32
@@ -11,6 +12,17 @@ start:
 
 	call set_up_page_tables
 	call enable_paging
+
+	; load the 64-bit gdt64
+	lgdt [gdt64.pointer]
+
+	; update selectors
+	mov ax, gdt64.data
+	mov ss, ax ; stack selector
+	mov ds, ax ; data selector
+	mov es, ax ; extra selector
+
+	jmp gdt64.code:long_mode_start
 
 	; print `OK` to screen
 	mov dword [0xb8000], 0x2f4b2f4f
@@ -133,6 +145,17 @@ enable_paging:
 	mov cr0, eax
 
 	ret
+
+section .rodata
+gdt64:
+	dq 0 ; zery entry
+.code: equ $ - gdt64
+	dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53) ; code segment
+.data: equ $ - gdt64
+	dq (1<<44) | (1<<47) | (1<<41) ; data segment
+.pointer:
+	dw $ - gdt64 - 1
+	dq gdt64
 
 section .bss
 align 4096
