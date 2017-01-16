494040103f3f232025b3f27306106c309006d7e8
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2017-01-16 16:27:52 +0700

    Step 5.1: print available memory from multiboot information structure

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@
 extern crate rlibc;
 extern crate volatile;
 extern crate spin;
+extern crate multiboot2;
 
 #[macro_use]
 mod vga_buffer;
@@ -13,8 +14,15 @@ mod vga_buffer;
 #[no_mangle]
 pub extern fn rust_main(multiboot_information_address: usize) {
 	vga_buffer::clear_screen();
-	println!("hello world{}", { println!("inner{}", {println!("more inner");"!"}); "!"});
+	let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
+	let memory_map_tag = boot_info.memory_map_tag()
+		.expect("Memory map tag required");
 
+	println!("memory areas:");
+	for area in memory_map_tag.memory_areas() {
+		println!("	start: 0x{:x}, length: 0x{:x}",
+			area.base_addr, area.length);
+	}
 	loop {};
 
 }
