deb44147b70b5ab9e09864a0b7ad082d7ae6c626
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2017-01-16 17:26:08 +0700

    Step 5.4: Start and End of Kernel

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,15 @@ pub extern fn rust_main(multiboot_information_address: usize) {
 		println!("    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
 			section.addr, section.size, section.flags);
 	}
+
+	let kernel_start = elf_sections_tag.sections().map(|s| s.addr)
+		.min().unwrap();
+	let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size)
+		.max().unwrap();
+	let multiboot_start = multiboot_information_address;
+	let multiboot_end = multiboot_start + (boot_info.total_size as usize);
+	println!("kernel_start: 0x{:x}, kernel_end: 0x{:x}", kernel_start, kernel_end);
+	println!("multiboo_start: 0x{:x}, multiboot_end: 0x{:x}", multiboot_start, multiboot_end);
 	loop {};
 
 }
